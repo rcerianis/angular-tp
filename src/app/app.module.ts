74457b8ef22432e0e605616e51484e5f0176eaf4
rcs <rcs@rcs.home>
2020-10-29 21:36:14 +0100

    Challenge 4 casa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { StarComponent } from './shared/ui/star/star.component';
+import { WelcomeComponent } from './welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    StarComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
