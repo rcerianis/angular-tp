9b30dce84d2718380aa9317881fce5551769edd1
rcs <rcs@rcs.home>
2020-11-02 20:58:45 +0100

    Custom pipe productSearch implemented.

--- src/app/product/product-search.pipe.ts
+++ src/app/product/product-search.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { IProduct } from './product-list/product-list.component';
 
 @Pipe({
   name: 'productSearch'
 })
 export class ProductSearchPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(products: IProduct[], searchTerm: string): IProduct[] {
+    const term: string = searchTerm.toLowerCase();
+
+    return products.filter((product: IProduct) => {
+      const name: string = product.productName.toLowerCase();
+      return name.indexOf(term) > -1;
+    });
   }
 
 }
